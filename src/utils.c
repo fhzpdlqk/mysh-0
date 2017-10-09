d98db3fc91c589993ab6763b11859cfdf7eb0e98
fhzpdlqk <rozencrants@naver.com>
2017-10-09 21:12:22 +0900

    finish testcast

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,8 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
@@ -6,15 +10,17 @@ void mysh_parse_command(const char* command,
   	char* copy = (char*)malloc(sizeof(char)*100);
 	strcpy(copy,command);
 	char* temp= (char*)malloc(sizeof(char)*100); 
-	temp = strtok(copy," \n");
+	temp = strtok(copy," \n\t");
 	*argc = 0;
-	printf("1");
 	*argv = (char**)malloc(sizeof(char)*100);
-	printf("2");
+	argv[0][0] = (char*)malloc(sizeof(char)*100);
+	memset(argv[0][0],NULL,100);
 	while(temp!=NULL){
 		argv[0][*argc] = (char*)malloc(sizeof(char)*100);
 		strcpy(argv[0][*argc],temp);
 		(*argc)++;
-		temp = strtok(NULL," \n");	
+		temp = strtok(NULL," \n\t");	
 	}
+	if(strcmp(argv[0][0],"")==0)
+		*argc=1;
 }
