9a837d610f21034aaa89fef9053886bf9ec2803b
fhzpdlqk <rozencrants@naver.com>
2017-10-09 20:17:19 +0900

    parshing method

--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,18 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
-  printf("123");
+  	char* copy = (char*)malloc(sizeof(char)*100);
+	strcpy(copy,command);
+	char* temp= (char*)malloc(sizeof(char)*100); 
+	temp = strtok(copy," \n");
+	*argc = 0;
+	printf("1");
+	*argv = (char**)malloc(sizeof(char)*100);
+	printf("2");
+	while(temp!=NULL){
+		argv[0][*argc] = (char*)malloc(sizeof(char)*100);
+		strcpy(argv[0][*argc],temp);
+		(*argc)++;
+		temp = strtok(NULL," \n");	
+	}
 }
